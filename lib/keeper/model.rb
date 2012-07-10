167d91095d585c8e28f58bb94843229c5fc580ac
Cristiano Betta <cbetta@gmail.com>
2012-07-11 00:26:00 +0100

    Remove model

--- lib/keeper/model.rb
+++
@@ -1,7 +0,0 @@
-class Keeper
-  class Model < ActiveResource::Base
-    self.site = Keeper.config["host"]
-    self.user = Keeper.config["user"]
-    self.password = Keeper.config["password"]
-  end
-end
