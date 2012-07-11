173ca9d02df291a3f67a6f6aac6b62d646b55977
Cristiano Betta <cbetta@gmail.com>
2012-07-12 00:56:12 +0100

    Added spec skeleton

--- lib/herder.rb
+++ lib/herder.rb
@@ -1,7 +1,9 @@
+require "active_resource"
+require "active_resource_pagination"
+
 require_relative "herder/config"
 require_relative "herder/model"
 require_relative "herder/attendee"
-require "active_resource_pagination"
 
 class Herder
 end
---
+++ spec/herder/attendee_spec.rb
@@ -0,0 +1,4 @@
+require "spec_helper"
+
+describe Herder::Attendee do
+end
